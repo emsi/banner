657160b7832128974ca3db804c7c8265a72d30d3
Mariusz Woloszyn <emsi@users.noreply.github.com>
2023-06-26 15:48:10 +0200

    apparently GitHub uses LA timezone and we're displaying progressbar

--- commit.py
+++ commit.py
@@ -2,10 +2,13 @@
 import subprocess
 from datetime import datetime, timedelta
 
+import pytz
+from tqdm import tqdm
+
 from txt_to_bitmap import text_to_array
 
 # number of commits in given date to be generated
-INTENSITY = 100
+INTENSITY = 1
 
 
 def first_sunday_after_date(date: str) -> datetime:
@@ -16,41 +19,40 @@ def first_sunday_after_date(date: str) -> datetime:
     """
 
     dt = datetime.strptime(date, "%Y-%m-%d")
-    return dt + timedelta(days=(6 - dt.weekday()) % 7)
+    sunday = dt + timedelta(days=(6 - dt.weekday()) % 7)
+    tz = pytz.timezone("America/Los_Angeles")
+    return tz.localize(sunday)
 
 
-# # Date in the format: "2022-06-27"
-# input_date = "2022-06-26"
-
-
-def commit_pixel(input_date):
+def commit_pixel(input_date: datetime):
     """Make commits at the given date"""
 
     # Convert the date into git format
-    dt = datetime.strptime(input_date, "%Y-%m-%d")
-    git_date = dt.strftime("%a %b %d %H:%M:%S %Y %z")
+    git_date = input_date.strftime("%a %b %d %H:%M:%S %Y %z")
 
     for i in range(INTENSITY):
         with open("file.txt", "a") as f:
             f.write(str(i) + "\n")
 
-        # # Stage changes
-        # subprocess.run(["git", "add", "."])
+        # Stage changes
+        subprocess.run(["git", "add", "."], stdout=subprocess.PIPE)
 
         # Make a commit with the specific date
-        subprocess.run(["git", "commit", "--date", git_date, "-m", f"commit {i}", "file.txt"])
+        subprocess.run(
+            ["git", "commit", "--date", git_date, "-m", f"commit {i}"], stdout=subprocess.PIPE
+        )
 
 
 def blit_banner(banner, start_date: str):
     """Blit the banner on the given date"""
     start_date = first_sunday_after_date(start_date)
 
-    for x in range(banner.shape[1]):
+    for x in tqdm(range(banner.shape[1]), desc="Blitting banner"):
         for y in range(banner.shape[0]):
             if banner[y, x] == "X":
                 date = start_date + timedelta(days=y) + timedelta(weeks=x)
-                print(date)
+                commit_pixel(date)
 
 
 if __name__ == "__main__":
-    blit_banner(text_to_array("EMSI.ME"), "2011-01-01")
+    blit_banner(text_to_array("EMSI.ME"), "2013-02-17")
