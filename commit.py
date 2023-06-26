a71b53fbb19ac826fe614ffa6d42b78484d58e5e
Mariusz Woloszyn <emsi@users.noreply.github.com>
2023-06-26 16:53:38 +0200

    use typer to make better cli, use empty commits

--- commit.py
+++ commit.py
@@ -3,12 +3,15 @@ import subprocess
 from datetime import datetime, timedelta
 
 import pytz
+import typer as typer
 from tqdm import tqdm
 
 from txt_to_bitmap import text_to_array
 
 # number of commits in given date to be generated
 INTENSITY = 1
+
+app = typer.Typer(add_completion=False)
 
 
 def first_sunday_after_date(date: str) -> datetime:
@@ -24,26 +27,29 @@ def first_sunday_after_date(date: str) -> datetime:
     return tz.localize(sunday)
 
 
-def commit_pixel(input_date: datetime):
+def commit_pixel(input_date: datetime, intensity: int, x: int, y: int):
     """Make commits at the given date"""
 
     # Convert the date into git format
     git_date = input_date.strftime("%a %b %d %H:%M:%S %Y %z")
 
-    for i in range(INTENSITY):
-        with open("file.txt", "a") as f:
-            f.write(str(i) + "\n")
-
-        # Stage changes
-        subprocess.run(["git", "add", "."], stdout=subprocess.PIPE)
-
+    for i in range(intensity):
         # Make a commit with the specific date
         subprocess.run(
-            ["git", "commit", "--date", git_date, "-m", f"commit {i}"], stdout=subprocess.PIPE
+            [
+                "git",
+                "commit",
+                "--date",
+                git_date,
+                "--allow-empty",
+                "-m",
+                f"pixel x:{x}, y:{y}, commit:{i}",
+            ],
+            stdout=subprocess.PIPE,
         )
 
 
-def blit_banner(banner, start_date: str):
+def blit_banner(banner, start_date: str, intensity: int):
     """Blit the banner on the given date"""
     start_date = first_sunday_after_date(start_date)
 
@@ -51,8 +57,20 @@ def blit_banner(banner, start_date: str):
         for y in range(banner.shape[0]):
             if banner[y, x] == "X":
                 date = start_date + timedelta(days=y) + timedelta(weeks=x)
-                commit_pixel(date)
+                commit_pixel(date, intensity, x, y)
+
+
+@app.command()
+def main(
+    banner_text: str = typer.Option("EMSI.ME", help="Text to be displayed on the commit graph."),
+    date: str = typer.Option(
+        "2013-02-17", help="Date from which the banner should start in the format: 'YYYY-MM-DD'"
+    ),
+    intensity: int = typer.Option(1, help="Number of commits per day"),
+):
+    """Generate a banner on the GitHub commit graph"""
+    blit_banner(text_to_array(banner_text), date, intensity)
 
 
 if __name__ == "__main__":
-    blit_banner(text_to_array("EMSI.ME"), "2013-02-17")
+    app()
